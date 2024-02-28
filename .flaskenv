69f49ac49100b8c7272eee1892ac1989305ec51e
alx <109251593+Sekani-kahlil@users.noreply.github.com>
2024-02-28 14:49:33 +0000

    blog development

---
+++ .flaskenv
@@ -0,0 +1,21 @@
+FLASK_APP=blogopedia.py
+FLASK_RUN_HOST=127.0.0.1
+FLASK_RUN_PORT=5000
+FLASK_DEBUG=1
+
+#MAIL_SERVER=smtp.gmail.com
+#MAIL_PORT=465
+#MAIL_USE_TLS=1
+#MAIL_USERNAME=efeeloobenedict@gmail.com
+#MAIL_PASSWORD='excz thct vjko sqtq'
+
+MAIL_SERVER=smtp.gmail.com
+MAIL_PORT=587
+MAIL_USE_TLS=1
+MAIL_USERNAME=mail.blogopedia@gmail.com
+MAIL_PASSWORD='Pistis 01 ?'
+
+GOOGLE_CLIENT_ID='369393828750-5tiavg5fujmgq5jkca018pn1fhmbspad.apps.googleusercontent.com'
+GOOGLE_CLIENT_SECRET='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'
+
+DATABASE_URL = "mysql+mysqlconnector://sql8686579:K7iGPpYjYh@sql8.freemysqlhosting.net/sql8686579"
---
+++ config.py
@@ -0,0 +1,32 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['efeeloobenedict@gmail.com', 'efeeloojr@gmail.com']
+
+    POSTS_PER_PAGE = 6
+
+    OAUTH2_PROVIDERS = {
+    # Google OAuth 2.0 documentation:
+    # https://developers.google.com/identity/protocols/oauth2/web-server#httprest
+    'google': {
+        'client_id': os.environ.get('GOOGLE_CLIENT_ID'),
+        'client_secret': os.environ.get('GOOGLE_CLIENT_SECRET'),
+        'authorize_url': 'https://accounts.google.com/o/oauth2/auth',
+        'token_url': 'https://accounts.google.com/o/oauth2/token',
+        'userinfo': {
+            'url': 'https://www.googleapis.com/oauth2/v3/userinfo',
+            'email': lambda json: json['email'],
+        },
+        'scopes': ['https://www.googleapis.com/auth/userinfo.email'],
+    },
+}
